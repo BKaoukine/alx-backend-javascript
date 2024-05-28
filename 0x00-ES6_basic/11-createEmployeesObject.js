34f452bde026aa271827f4d3f23c22c4493dacde
bkaoukine <b.koukin@gmail.com>
2024-05-28 19:43:13 +0100

    0. Keep every promise you make and only make promises you can keep

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,11 @@
+export default function createEmployeesObject(departmentName, employees) {
+  const worker = employees;
+  function iteratorarra(worker) {
+    let employee;
+    employees.map(worker);
+    return employee;
+  }
+  return {
+    [departmentName]: [[iteratorarra]],
+  };
+}
---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,5 @@
+export default function getResponseFromAPI() {
+  return new Promise(() => {
+    // Empty promise, no resolve or reject
+  });
+}
