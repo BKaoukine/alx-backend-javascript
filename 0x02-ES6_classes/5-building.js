f69fddce61ea2c546cb116c22deaa7cab5fc5253
bkaoukine <b.koukin@gmail.com>
2024-05-31 18:57:25 +0100

    5. A Building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,20 @@
+/* eslint no-underscore-dangle: 0 */
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (
+      !Object.prototype.hasOwnProperty.call(this, 'evacuationWarningMessage')
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  static evacuationWarningMessage() {}
+}
