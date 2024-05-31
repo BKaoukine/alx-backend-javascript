1d523e7a611efcd37e6abacd8c0ed9d1720166d2
bkaoukine <b.koukin@gmail.com>
2024-06-01 00:39:44 +0100

    10. Vroom

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,11 @@
+export default class Car {
+  Constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
