4e6fff85581c476a706173c04321b65e39ca8f74
bkaoukine <b.koukin@gmail.com>
2024-06-05 00:01:16 +0100

    5. Typed Arrays

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,11 @@
+const createInt8TypedArray = (length, position, value) => {
+  if (position < 0 || position >= length) {
+    throw Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const view = new Int8Array(buffer);
+  view.set([value], position);
+  return new DataView(buffer);
+};
+
+export default createInt8TypedArray;
