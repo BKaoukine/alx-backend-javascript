4e51ffddceb086b6a37c3882df416586e96db767
bkaoukine <b.koukin@gmail.com>
2024-07-23 13:05:40 +0100

    adding files..

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-  res.write('Hello Holberton School!');
-  res.end();
-}).listen(1245);
+const hostname = '127.0.0.1';
+const port = 1245;
 
-exports.app = app;
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello Holberton School!');
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
